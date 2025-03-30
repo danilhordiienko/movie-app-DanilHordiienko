904d60d1e61d5553e626b6ac0b8e8d87f0ecb15d
danilhordiienko <165608127+danilhordiienko@users.noreply.github.com>
2025-03-30 21:03:10 +0000

    I added a logic fot the film displaying

--- movie-app/src/app/home/home.page.ts
+++ movie-app/src/app/home/home.page.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { MovieService } from '../services/movie.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
 })
-export class HomePage {
-  constructor() {}
+export class HomePage implements OnInit {
+  movies: any[] = [];
+
+  constructor(private movieService: MovieService) {}
+
+  ngOnInit() {
+    this.movieService.getMovies().subscribe(data => {
+      this.movies = data.Search || [];
+    });
+  }
 }
